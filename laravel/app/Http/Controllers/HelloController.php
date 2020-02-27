1cef234171872ffc6d1120999f9d45716b9d8adb
tomoka-kosugi <kosugi@teinei.co.jp>
2020-02-27 17:49:16 +0900

    2/27進捗

--- laravel/app/Http/Controllers/HelloController.php
+++ laravel/app/Http/Controllers/HelloController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['data'=>$request->data]);
+        return view('hello.index', ['msg'=>'フォームを入力：']);
+    }
+
+    public function post(HelloRequest $request)
+    {
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
