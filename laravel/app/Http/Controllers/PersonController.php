7b90800220d30a7658e09f2c94110c90983324f1
tomoka-kosugi <kosugi@teinei.co.jp>
2020-03-06 17:39:57 +0900

    3/6進捗

---
+++ laravel/app/Http/Controllers/PersonController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function index(Request $request)
+    {
+        $items = Person::all();
+        return view('Person.index', ['items' => $items]);
+    }
+
+    public function find(Request $request)
+    {
+        return view('Person.index', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $item = Person::where('name', $request->input)->first();
+        $param = ['input' => $request->input, 'item' => $item];
+        return view('person.find', $param);
+    }
+    //
+}
