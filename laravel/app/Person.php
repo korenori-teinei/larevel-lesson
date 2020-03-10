27948ee1b53f2767ce9b2917bdc0315c81149ad7
tomoka-kosugi <kosugi@teinei.co.jp>
2020-03-10 17:20:23 +0900

    no message

--- laravel/app/Person.php
+++ laravel/app/Person.php
@@ -3,11 +3,39 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
+
 
 class Person extends Model
 {
     public function getData()
     {
-        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')' . ' : ' . $this->mail;
+    }
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
+    public function setUpdatedAt($value)
+    {
+        return $this;
+    }
+
+    public function setCreatedAt($value)
+    {
+        return $this;
+    }
+
+    public $timestamps = false;
+
+    public function boards()
+    {
+        return $this->hasMany('App\Board');
     }
 }
